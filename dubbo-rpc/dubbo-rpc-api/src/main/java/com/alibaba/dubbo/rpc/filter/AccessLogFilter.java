5b54821b4e6aea7b56baec22597e6f1bdb443b73
lstoars <lstoars@gmail.com>
2016-09-24 10:18:08 +0800

    日志过滤器修改.

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/AccessLogFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/AccessLogFilter.java
@@ -32,7 +32,6 @@ import java.util.concurrent.TimeUnit;
 
 import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.extension.Activate;
-import com.alibaba.dubbo.common.json.JSON;
 import com.alibaba.dubbo.common.logger.Logger;
 import com.alibaba.dubbo.common.logger.LoggerFactory;
 import com.alibaba.dubbo.common.utils.ConcurrentHashSet;
@@ -44,13 +43,13 @@ import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.Result;
 import com.alibaba.dubbo.rpc.RpcContext;
 import com.alibaba.dubbo.rpc.RpcException;
+import com.alibaba.fastjson.JSON;
 
 /**
  * 记录Service的Access Log。
  * <p/>
- * 使用的Logger key是<code><b>dubbo.accesslog</b></code>。
- * 如果想要配置Access Log只出现在指定的Appender中，可以在Log4j中注意配置上additivity。配置示例:
- * <code>
+ * 使用的Logger key是<code><b>dubbo.accesslog</b></code>。 如果想要配置Access
+ * Log只出现在指定的Appender中，可以在Log4j中注意配置上additivity。配置示例: <code>
  * <pre>
  * &lt;logger name="<b>dubbo.accesslog</b>" <font color="red">additivity="false"</font>&gt;
  *    &lt;level value="info" /&gt;
@@ -63,152 +62,164 @@ import com.alibaba.dubbo.rpc.RpcException;
 @Activate(group = Constants.PROVIDER, value = Constants.ACCESS_LOG_KEY)
 public class AccessLogFilter implements Filter {
 
-    private static final Logger logger = LoggerFactory.getLogger(AccessLogFilter.class);
+	private static final Logger logger = LoggerFactory.getLogger(AccessLogFilter.class);
 
-    private static final String ACCESS_LOG_KEY = "dubbo.accesslog";
+	private static final String ACCESS_LOG_KEY = "dubbo.accesslog";
 
-    private static final String FILE_DATE_FORMAT = "yyyyMMdd";
+	private static final String FILE_DATE_FORMAT = "yyyyMMdd";
 
-    private static final int LOG_MAX_BUFFER = 5000;
+	private static final int LOG_MAX_BUFFER = 5000;
 
-    private static final long LOG_OUTPUT_INTERVAL = 5000;
+	private static final long LOG_OUTPUT_INTERVAL = 5000;
 
-    private final ConcurrentMap<String, Set<String>> logQueue = new ConcurrentHashMap<String, Set<String>>();
+	private final ConcurrentMap<String, Set<String>> logQueue = new ConcurrentHashMap<String, Set<String>>();
 
-    private final ScheduledExecutorService logScheduled = Executors.newScheduledThreadPool(2, new NamedThreadFactory("Dubbo-Access-Log", true));
+	private final ScheduledExecutorService logScheduled = Executors.newScheduledThreadPool(2, new NamedThreadFactory(
+			"Dubbo-Access-Log", true));
 
-    private volatile ScheduledFuture<?> logFuture = null;
+	private volatile ScheduledFuture<?> logFuture = null;
 
-    private class LogTask implements Runnable {
-        public void run() {
-            try {
-                if (logQueue != null && logQueue.size() > 0) {
-                    for (Map.Entry<String, Set<String>> entry : logQueue.entrySet()) {
-                        try {
-                            String accesslog = entry.getKey();
-                            Set<String> logSet = entry.getValue();
-                            File file = new File(accesslog);
-                            File dir = file.getParentFile();
-                            if (null != dir && !dir.exists()) {
-                                dir.mkdirs();
-                            }
-                            if (logger.isDebugEnabled()) {
-                                logger.debug("Append log to " + accesslog);
-                            }
-                            if (file.exists()) {
-                                String now = new SimpleDateFormat(FILE_DATE_FORMAT).format(new Date());
-                                String last = new SimpleDateFormat(FILE_DATE_FORMAT).format(new Date(file.lastModified()));
-                                if (!now.equals(last)) {
-                                    File archive = new File(file.getAbsolutePath() + "." + last);
-                                    file.renameTo(archive);
-                                }
-                            }
-                            FileWriter writer = new FileWriter(file, true);
-                            try {
-                                for (Iterator<String> iterator = logSet.iterator();
-                                     iterator.hasNext();
-                                     iterator.remove()) {
-                                    writer.write(iterator.next());
-                                    writer.write("\r\n");
-                                }
-                                writer.flush();
-                            } finally {
-                                writer.close();
-                            }
-                        } catch (Exception e) {
-                            logger.error(e.getMessage(), e);
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                logger.error(e.getMessage(), e);
-            }
-        }
-    }
-
-    private void init() {
-        if (logFuture == null) {
-            synchronized (logScheduled) {
-                if (logFuture == null) {
-                    logFuture = logScheduled.scheduleWithFixedDelay(new LogTask(), LOG_OUTPUT_INTERVAL, LOG_OUTPUT_INTERVAL, TimeUnit.MILLISECONDS);
-                }
-            }
-        }
-    }
-
-    private void log(String accesslog, String logmessage) {
-        init();
-        Set<String> logSet = logQueue.get(accesslog);
-        if (logSet == null) {
-            logQueue.putIfAbsent(accesslog, new ConcurrentHashSet<String>());
-            logSet = logQueue.get(accesslog);
-        }
-        if (logSet.size() < LOG_MAX_BUFFER) {
-            logSet.add(logmessage);
-        }
-    }
-
-    public Result invoke(Invoker<?> invoker, Invocation inv) throws RpcException {
-		StringBuilder sn = new StringBuilder();
-        String accesslog = invoker.getUrl().getParameter(Constants.ACCESS_LOG_KEY);
-        long begin = System.currentTimeMillis();
-        try {
-            if (ConfigUtils.isNotEmpty(accesslog)) {
-                RpcContext context = RpcContext.getContext();
-                String serviceName = invoker.getInterface().getName();
-                String version = invoker.getUrl().getParameter(Constants.VERSION_KEY);
-                String group = invoker.getUrl().getParameter(Constants.GROUP_KEY);
-
-                sn.append(context.getRemoteHost()).append(":").append(context.getRemotePort())
-                        .append(" -> ").append(context.getLocalHost()).append(":").append(context.getLocalPort())
-                        .append(" - ");
-                if (null != group && group.length() > 0) {
-                    sn.append(group).append("/");
-                }
-                sn.append(serviceName);
-                if (null != version && version.length() > 0) {
-                    sn.append(":").append(version);
-                }
-                sn.append(" ");
-                sn.append(inv.getMethodName());
-                sn.append("(");
-                Class<?>[] types = inv.getParameterTypes();
-                if (types != null && types.length > 0) {
-                    boolean first = true;
-                    for (Class<?> type : types) {
-                        if (first) {
-                            first = false;
-                        } else {
-                            sn.append(",");
-                        }
-                        sn.append(type.getName());
-                    }
-                }
-                sn.append(") ");
-                Object[] args = inv.getArguments();
-                if (args != null && args.length > 0) {
-                    sn.append("params:").append(JSON.json(args));
-                }
-            }
-        } catch (Throwable t) {
-            logger.warn("Exception in AccessLogFilter of service(" + invoker + " -> " + inv + ")", t);
-        }
-        Result result = invoker.invoke(inv);
-        if (ConfigUtils.isNotEmpty(accesslog)) {
-            long end = System.currentTimeMillis();
-            try {
-                sn.append(" response:[").append(JSON.json(result)).append("]");
-            } catch (IOException e) {
-                logger.warn("Exception in AccessLogFilter result to json", e);
-            }
-            sn.append(" costs:").append(end - begin);
-			if (ConfigUtils.isDefault(accesslog)) {
-				LoggerFactory.getLogger(ACCESS_LOG_KEY + "." + invoker.getInterface().getName()).info(sn.toString());
-			} else {
-				LoggerFactory.getLogger(accesslog).info(sn.toString());
+	private class LogTask implements Runnable {
+		public void run() {
+			try {
+				if (logQueue != null && logQueue.size() > 0) {
+					for (Map.Entry<String, Set<String>> entry : logQueue.entrySet()) {
+						try {
+							String accesslog = entry.getKey();
+							Set<String> logSet = entry.getValue();
+							File file = new File(accesslog);
+							File dir = file.getParentFile();
+							if (null != dir && !dir.exists()) {
+								dir.mkdirs();
+							}
+							if (logger.isDebugEnabled()) {
+								logger.debug("Append log to " + accesslog);
+							}
+							if (file.exists()) {
+								String now = new SimpleDateFormat(FILE_DATE_FORMAT).format(new Date());
+								String last = new SimpleDateFormat(FILE_DATE_FORMAT).format(new Date(file
+										.lastModified()));
+								if (!now.equals(last)) {
+									File archive = new File(file.getAbsolutePath() + "." + last);
+									file.renameTo(archive);
+								}
+							}
+							FileWriter writer = new FileWriter(file, true);
+							try {
+								for (Iterator<String> iterator = logSet.iterator(); iterator.hasNext(); iterator
+										.remove()) {
+									writer.write(iterator.next());
+									writer.write("\r\n");
+								}
+								writer.flush();
+							} finally {
+								writer.close();
+							}
+						} catch (Exception e) {
+							logger.error(e.getMessage(), e);
+						}
+					}
+				}
+			} catch (Exception e) {
+				logger.error(e.getMessage(), e);
 			}
-        }
-        return result;
-    }
+		}
+	}
+
+	private void init() {
+		if (logFuture == null) {
+			synchronized (logScheduled) {
+				if (logFuture == null) {
+					logFuture = logScheduled.scheduleWithFixedDelay(new LogTask(), LOG_OUTPUT_INTERVAL,
+							LOG_OUTPUT_INTERVAL, TimeUnit.MILLISECONDS);
+				}
+			}
+		}
+	}
+
+	private void log(String accesslog, String logmessage) {
+		init();
+		Set<String> logSet = logQueue.get(accesslog);
+		if (logSet == null) {
+			logQueue.putIfAbsent(accesslog, new ConcurrentHashSet<String>());
+			logSet = logQueue.get(accesslog);
+		}
+		if (logSet.size() < LOG_MAX_BUFFER) {
+			logSet.add(logmessage);
+		}
+	}
+
+	public Result invoke(Invoker<?> invoker, Invocation inv) throws RpcException {
+		StringBuilder sn = new StringBuilder();
+		String accesslog = invoker.getUrl().getParameter(Constants.ACCESS_LOG_KEY);
+		long begin = System.currentTimeMillis();
+		try {
+			if (ConfigUtils.isNotEmpty(accesslog)) {
+				RpcContext context = RpcContext.getContext();
+				String serviceName = invoker.getInterface().getName();
+				String version = invoker.getUrl().getParameter(Constants.VERSION_KEY);
+				String group = invoker.getUrl().getParameter(Constants.GROUP_KEY);
+
+				sn.append(context.getRemoteHost()).append(":").append(context.getRemotePort()).append(" -> ")
+						.append(context.getLocalHost()).append(":").append(context.getLocalPort()).append(" - ");
+				if (null != group && group.length() > 0) {
+					sn.append(group).append("/");
+				}
+				sn.append(serviceName);
+				if (null != version && version.length() > 0) {
+					sn.append(":").append(version);
+				}
+				sn.append(" ");
+				sn.append(inv.getMethodName());
+				sn.append("(");
+				Class<?>[] types = inv.getParameterTypes();
+				if (types != null && types.length > 0) {
+					boolean first = true;
+					for (Class<?> type : types) {
+						if (first) {
+							first = false;
+						} else {
+							sn.append(",");
+						}
+						sn.append(type.getName());
+					}
+				}
+				sn.append(") ");
+				Object[] args = inv.getArguments();
+				if (args != null && args.length > 0) {
+					sn.append("params:").append(JSON.toJSONString(args));
+				}
+			}
+		} catch (Throwable t) {
+			logger.warn("Exception in AccessLogFilter of service(" + invoker + " -> " + inv + ")", t);
+		}
+		Result result = invoker.invoke(inv);
+		if (ConfigUtils.isNotEmpty(accesslog)) {
+			long end = System.currentTimeMillis();
+			try {
+				String resp = JSON.toJSONString(result);
+				if (resp != null && resp.length() > 1000) {
+					resp = resp.substring(0, 1000);
+				}
+				sn.append(" response:[").append(resp).append("]");
+			} catch (Exception e) {
+				logger.warn("Exception in AccessLogFilter result to json", e);
+			}
+			sn.append(" costs:").append(end - begin);
+			Logger logger;
+			if (ConfigUtils.isDefault(accesslog)) {
+				logger = LoggerFactory.getLogger(ACCESS_LOG_KEY + "." + invoker.getInterface().getName());
+				if (logger.isInfoEnabled()) {
+					logger.info(sn.toString());
+				}
+			} else {
+				logger = LoggerFactory.getLogger(accesslog);
+				if (logger.isInfoEnabled()) {
+					logger.info(sn.toString());
+				}
+			}
+		}
+		return result;
+	}
 
 }
