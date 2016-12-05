1435852ad5c2503d28daa60eeecc10dd693da33b
欧阳文斌 <ouyangwenbin2009@live.cn>
2016-12-05 14:35:46 +0800

    新增IP查询提供者和消费者的http接口

--- dubbo-admin/src/main/java/com/alibaba/dubbo/governance/web/sysinfo/module/screen/Warns.java
+++ dubbo-admin/src/main/java/com/alibaba/dubbo/governance/web/sysinfo/module/screen/Warns.java
@@ -1,7 +1,13 @@
 package com.alibaba.dubbo.governance.web.sysinfo.module.screen;
 
 import java.io.IOException;
-import java.util.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,6 +17,9 @@ import com.alibaba.dubbo.common.utils.StringUtils;
 import com.alibaba.dubbo.governance.service.ConsumerService;
 import com.alibaba.dubbo.governance.service.ProviderService;
 import com.alibaba.dubbo.governance.web.common.module.screen.Restful;
+import com.alibaba.dubbo.registry.common.domain.Consumer;
+import com.alibaba.dubbo.registry.common.domain.Provider;
+import com.alibaba.fastjson.JSONArray;
 
 /**
  * Created by lenovo on 2016/3/3.
@@ -54,5 +63,67 @@ public class Warns extends Restful {
 			response.getWriter().println("1");
 		}
 
+	}
+
+	/**  
+	 * @功能描述: 根据IP查询服务器的提供接口以及消费接口
+	 * @创建作者: 欧阳文斌
+	 * @创建日期: 2016年12月5日 上午10:28:42
+	 * @param ip
+	 * @throws IOException
+	 */ 
+	public void queryByIp(Map<String, Object> context) throws IOException {
+		HashMap<String, Object> result = new HashMap<String, Object>();
+		if(context==null){result.put("status", -1);result.put("msg", "参数错误");}
+		String ip = (String) context.get("ip");
+		if(StringUtils.isEmpty(ip)){result.put("status", -1);result.put("msg", "参数错误");}
+		String port = (String) context.get("port");
+		if(StringUtils.isEmpty(port)) port = "20880";
+		String providerIp = ip+":"+port;
+		//默认端口  如果一台服务器多个提供者，需要拓展这里的方法
+		List<Provider> providers= providerService.findByAddress(providerIp);
+		List<Consumer> consumers= consumerService.findByAddress(ip);
+
+		List<CheckServiceModel> providerResult = new ArrayList<Warns.CheckServiceModel>();
+		if(providers!=null){
+			for (Provider provider : providers) {
+				CheckServiceModel model = new CheckServiceModel();
+				model.setUrl(provider.getUrl());
+				providerResult.add(model);
+			}
+		}
+		result.put("providerDatas", providerResult);
+		List<CheckServiceModel> consumerResult = new ArrayList<Warns.CheckServiceModel>();
+		if(consumers!=null){
+			for (Consumer consumer : consumers) {
+				CheckServiceModel model = new CheckServiceModel();
+				model.setService(consumer.getService());
+				consumerResult.add(model);
+			}
+		}
+		result.put("consumerDatas", consumerResult);
+		result.put("status", 1);result.put("msg", "调用成功");
+		response.getWriter().println(JSONArray.toJSONString(result));
+
+	}
+	
+	class CheckServiceModel implements Serializable{
+		private static final long serialVersionUID = 1L;
+		/** dubbo url */
+		private String url;
+		/** 接口 */
+		private String service;
+		public String getUrl() {
+			return url;
+		}
+		public void setUrl(String url) {
+			this.url = url;
+		}
+		public String getService() {
+			return service;
+		}
+		public void setService(String service) {
+			this.service = service;
+		}
 	}
 }
